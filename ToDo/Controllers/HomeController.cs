25c8b87d899a1decd72e518cc6ab56097c76f723
IREMTARGıT\irem turgut <iremt.151@gmail.com>
2024-05-08 17:05:11 +0300

    Proje dosyası ekle.

---
+++ ToDo/Controllers/HomeController.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using ToDoDemo.Models;
+
+namespace ToDoDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private ToDoContext context;
+
+        public HomeController(ToDoContext ctx) => context = ctx;
+        public IActionResult Index(string id)
+        {
+            var filters = new Filters(id);
+            ViewBag.Filters = filters;
+
+            ViewBag.Categories = context.Categories.ToList();
+            ViewBag.Statuses = context.Statuses.ToList();
+            ViewBag.DuesFilters = Filters.DueFilterValues;//this is property
+
+            IQueryable<ToDo> query = context.ToDos
+                .Include(t => t.Category)
+                .Include(t => t.Status);//db name
+
+            if (filters.HasCategory)
+            {
+                query = query.Where(t => t.CategoryId == filters.CategoryId);
+            }
+            if (filters.HasStatus)
+            {
+                query = query.Where(t => t.StatusId == filters.StatusId);
+            }
+            if (filters.HasDue)
+            {
+                var today = DateTime.Today;
+                if (filters.IsPast)
+                {
+                    query = query.Where(t => t.DueDate < today);
+                }
+                else if (filters.IsFuture)
+                {
+                    query = query.Where(t => t.DueDate > today);
+                }
+                else if (filters.IsToday)
+                {
+                    query = query.Where(t => t.DueDate == today);
+                }
+
+            }
+            var tasks = query.OrderBy(t => t.DueDate).ToList();
+
+            return View(tasks);
+        }
+        [HttpGet]
+        public IActionResult Add()//for add new todo
+        {
+            ViewBag.Categories = context.Categories.ToList();
+            ViewBag.Statuses = context.Statuses.ToList();
+            var task = new ToDo { StatusId = "open" };
+
+            return View(task);
+        }
+
+        [HttpPost]//redirection to the home
+        public IActionResult Add(ToDo task)//for add new todo
+        {
+            if (ModelState.IsValid)
+            {
+                context.ToDos.Add(task);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Categories = context.Categories.ToList();
+                ViewBag.Statuses = context.Statuses.ToList();
+                return View(task);//it came from reloads the viewback
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Filter(string[] filter)
+        {
+            string id = string.Join('-', filter);//- ye göre ayır
+            return RedirectToAction("Index", new { ID = id });
+        }
+
+        [HttpPost]
+        public IActionResult MarkComplete([FromRoute] string id, ToDo selected)//get id from url(fromroute)
+        {
+            selected = context.ToDos.Find(selected.Id)!;
+
+            if (selected != null)
+            {
+                selected.StatusId = "closed";
+                context.SaveChanges();
+
+            }
+            return RedirectToAction("Index", new { ID = id });
+        }
+
+        [HttpPost]
+        public IActionResult DeleteComplete(string id)
+        {
+            var toDelete = context.ToDos.Where(t => t.StatusId == "closed").ToList();
+
+            foreach (var task in toDelete)
+            {
+                context.ToDos.Remove(task);
+            }
+            context.SaveChanges();
+
+            return RedirectToAction("Index", new { ID = id });
+        }
+    }
+}
